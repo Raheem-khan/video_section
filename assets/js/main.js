54e1a1f9e40cabfb399b8113197820fa3aa4ade4
Rahim <rk.khan71@gmail.com>
2017-11-14 01:01:07 +0100

    video sec

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+		$(".video-play-btn").magnificPopup({
+			type: 'video',
+		});
+
+
+    })
+
+
+    jQury(window).load(funtion(){
+
+        
+    });
+
+
+}(jQuery));	
